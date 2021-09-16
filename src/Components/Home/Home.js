edbba5935619c06c9518f27f84af2c7104d8f4c9
Mohammad Eusha <eusha37@gmail.com>
2021-09-16 20:25:07 +0600

    jdha

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -34,13 +34,13 @@ const Home = () => {
     }
 
     return (
-        <div>
-            <div class="card text-secondary border border-info border-2 rounded focus mt-5">
+        <div >
+            <div class="card text-secondary border border-danger border-2 rounded focus mt-5">
 
                 <div class="p-5">
                     <div className="row">
                         <div className="col-md-4">
-                            <div class="card h-100 mb-3 card-bg text-dark border border-info border-2 rounded focus">
+                            <div class="card h-100 mb-3 card-bg text-dark border border-danger border-2 rounded focus">
                                 <div className="text-center">
                                     <img src={Chocolate} className="card-img-top img-fluid w-50 h-100 text-center" alt="..." />
                                 </div>
@@ -53,14 +53,14 @@ const Home = () => {
                                         <h5>Product Price: $</h5>
                                         <h5 className="">100</h5>
                                     </div>
-                                    <button onClick={(e) => handleBuy(e)} type="button" className="btn btn-secondary col-md-6 rounded-pill   "><FontAwesomeIcon icon={faCartPlus} />  Buy Now</button>
+                                    <button onClick={(e) => handleBuy(e)} type="button" className="btn btn-danger col-md-6 rounded-pill   "><FontAwesomeIcon icon={faCartPlus} />  Buy Now</button>
 
 
                                 </div>
                             </div>
                         </div>
                         <div className="col-md-4">
-                            <div class="card h-100 mb-3 card-bg text-dark border border-info border-2 rounded focus">
+                            <div class="card h-100 mb-3 card-bg text-dark border border-danger border-2 rounded focus">
                                 <div className="text-center">
                                     <img src={Milk} className="card-img-top img-fluid w-50 h-100" alt="..." />
                                 </div>
@@ -74,13 +74,13 @@ const Home = () => {
                                         <h5 className="">200</h5>
                                     </div>
 
-                                    <button onClick={(e) => console.log('click')} type="button" className="btn btn-secondary col-md-6 rounded-pill   "><FontAwesomeIcon icon={faCartPlus} />  Buy Now</button>
+                                    <button onClick={(e) => console.log('click')} type="button" className="btn btn-danger col-md-6 rounded-pill   "><FontAwesomeIcon icon={faCartPlus} />  Buy Now</button>
 
                                 </div>
                             </div>
                         </div>
                         <div className="col-md-4">
-                            <div class="card h-100 mb-3 card-bg text-dark border border-info border-2 rounded focus">
+                            <div class="card h-100 mb-3 card-bg text-dark border border-danger border-2 rounded focus">
                                 <div className="text-center">
                                     <img src={tang} className="card-img-top img-fluid w-50 h-100" alt="..." />
                                 </div>
@@ -94,7 +94,7 @@ const Home = () => {
                                         <h5 className="">300</h5>
                                     </div>
 
-                                    <button onClick={(e) => console.log('click')} type="button" className="btn btn-secondary col-md-6 rounded-pill   "><FontAwesomeIcon icon={faCartPlus} />  Buy Now</button>
+                                    <button onClick={(e) => console.log('click')} type="button" className="btn btn-danger col-md-6 rounded-pill   "><FontAwesomeIcon icon={faCartPlus} />  Buy Now</button>
 
 
                                 </div>
@@ -105,14 +105,14 @@ const Home = () => {
             </div>
             <div className="row mt-1">
                 <div className="col-md-6">
-                    <div class="input-group mb-3 border border-info border-2 rounded">
+                    <div class="input-group mb-3 border border-danger border-2 rounded">
                         <input onChange={(e) => setProductName(e.target.value)} type="text" class="form-control" placeholder="Name" aria-label="Recipient's username" aria-describedby="basic-addon2" />
                         <span class="input-group-text" id="basic-addon2">Product Name</span>
                     </div>
                 </div>
 
                 <div className="col-md-6">
-                    <div class="input-group mb-3 col-md-6 border border-info border-2 rounded">
+                    <div class="input-group mb-3 col-md-6 border border-danger border-2 rounded">
                         <span class="input-group-text">$</span>
                         <input onChange={(e) => setUserPrice(e.target.value)} type="number" class="form-control" placeholder="Price" aria-label="Amount (to the nearest dollar)" />
                         <span class="input-group-text">.00</span>
